06da8bfcf817eef22ba08255f74ab48530598982
Pedro Edilson <pedroedilson17@gmail.com>
2023-09-16 17:31:57 -0300

    foi_tualizado
    eu n tenho equipe

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const modal_overlay =  document.querySelector ('.modal_overlay');
+
+const cards = document.querySelectorAll ('.card');
+
+for (let card of cards) {
+card.addEventListener ("click", function()
+{
+    const videoid = card.getAttribute("id")
+    modal_overlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoid}`
+    modal_overlay.classList.add('active')
+})
+};
+
+document.querySelector('.close_modal').addEventListener('click', function(){
+    modal_overlay.classList.remove('active')
+});
